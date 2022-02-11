915f502a8f24098a1b5cc278f7790c835ed0407d
Alexandros <alexandr.kyriakou@gmail.com>
2022-02-11 18:03:42 +0200

    hero

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,20 @@
 const toggle = document.querySelector(".mobile-toggle");
 const mobileMenu = document.querySelector(".nav");
 
+// Menu Toggle
 toggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
+});
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
 });
