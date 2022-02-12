98c7f61b1987262c37e9c8f6c045be98b2e0e72d
Alexandros <alexandr.kyriakou@gmail.com>
2022-02-12 19:54:28 +0200

    hero section moving prices

--- app/js/app.js
+++ app/js/app.js
@@ -18,3 +18,16 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// Moving Buttons
+const heroSection = document.querySelector(".hero");
+
+heroSection.addEventListener("mousemove", (e) => {
+  heroSection.querySelectorAll(".btn").forEach((btn) => {
+    const speed = btn.getAttribute("data-speed");
+    const percent = 300;
+    const x = (window.innerWidth - e.pageX * speed) / percent;
+    const y = (window.innerHeight - e.pageY * speed) / percent;
+    btn.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+});
